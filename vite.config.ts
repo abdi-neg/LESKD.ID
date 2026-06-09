475c4e124d53c16e96a290744725774d0c6f4fc4
abdi-neg <152793289+abdi-neg@users.noreply.github.com>
2026-06-09 22:57:48 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // 🌟 TAMBAHKAN BARIS INI: Mengunci rute pencarian asset agar tidak tersesat di sub-URL
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
